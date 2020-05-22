76398fafdd0937a80c4e90f664bc71ac9f591683
alisdlyc <1428717646@qq.com>
2020-05-22 15:57:15 +0800

    N叉树的层序遍历

---
+++ src/com/alisdlyc/leetcode/editor/cn/[429]N叉树的层序遍历.java
@@ -0,0 +1,98 @@
+package com.alisdlyc.leetcode.editor.cn;//给定一个 N 叉树，返回其节点值的层序遍历。 (即从左到右，逐层遍历)。
+//
+// 例如，给定一个 3叉树 : 
+//
+// 
+//
+// 
+//
+// 
+//
+// 返回其层序遍历: 
+//
+// [
+//     [1],
+//     [3,2,4],
+//     [5,6]
+//]
+// 
+//
+// 
+//
+// 说明: 
+//
+// 
+// 树的深度不会超过 1000。 
+// 树的节点总数不会超过 5000。 
+// Related Topics 树 广度优先搜索
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+/*
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Solution {
+    private List<List<Integer>> re;
+    public List<List<Integer>> levelOrder(Node root) {
+        re = new ArrayList<>();
+        dfs(root);
+        return re;
+    }
+
+    public void dfs(Node root) {
+        if (root == null) {
+            return;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node node = queue.poll();
+                assert node != null;
+                level.add(node.val);
+                queue.addAll(node.children);
+            }
+            re.add(level);
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};*/
