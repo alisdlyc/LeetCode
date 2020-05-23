36bfca6cab017067933e244207b1618503ec7341
alisdlyc <1428717646@qq.com>
2020-05-23 10:00:20 +0800

    两数相加的链表实现

---
+++ src/com/alisdlyc/leetcode/editor/cn/[2]两数相加.java
@@ -0,0 +1,58 @@
+//给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。 
+//
+// 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。 
+//
+// 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。 
+//
+// 示例： 
+//
+// 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+//输出：7 -> 0 -> 8
+//原因：342 + 465 = 807
+// 
+// Related Topics 链表 数学
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import javax.print.attribute.standard.NumberUp;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+/*class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode c1 = l1;
+        ListNode c2 = l2;
+        ListNode sentinel = new ListNode(0);
+        ListNode d = sentinel;
+        int sum = 0;
+        while (c1 != null || c2 != null) {
+            sum /= 10;
+            if (c1 != null) {
+                sum += c1.val;
+                c1 = c1.next;
+            }
+
+            if (c2 != null) {
+                sum += c2.val;
+                c2 = c2.next;
+            }
+
+            d.next = new ListNode(sum % 10);
+            System.out.println(sum);
+            d = d.next;
+        }
+        if (sum / 10 == 1) {
+            d.next = new ListNode(1);
+        }
+        return sentinel.next;
+    }
+
+}*/
+//leetcode submit region end(Prohibit modification and deletion)
